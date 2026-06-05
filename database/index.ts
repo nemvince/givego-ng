bcd4000011b4dc2883dd066dc391a3d52bd13a85
Tamas Vince <nemvince@proton.me>
2026-06-05 22:50:59 +0200

    fix: update database connection handling for production and development environments

--- database/index.ts
+++ database/index.ts
@@ -1,4 +1,21 @@
-import { drizzle } from "drizzle-orm/bun-sql";
 import { env } from "@/lib/env";
+import { SQL } from "bun";
+import { drizzle } from "drizzle-orm/bun-sql";
 
-export const db = drizzle(env.DATABASE_URL);
+let connection: SQL;
+
+if (process.env.NODE_ENV === "production") {
+  connection = new SQL(env.DATABASE_URL);
+} else {
+  const globalConnection = global as typeof globalThis & {
+    connection: SQL;
+  };
+
+  if (!globalConnection.connection) {
+    globalConnection.connection = new SQL(env.DATABASE_URL);
+  }
+
+  connection = globalConnection.connection;
+}
+
+export const db = drizzle(connection, {});
