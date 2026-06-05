1a1afc826c2c1a20310fd3eb737428e72ed870f7
Tamas Vince <nemvince@proton.me>
2026-06-06 00:05:19 +0200

    feat: add script to wipe all objects in S3 bucket

---
+++ scripts/wipe-bucket.ts
@@ -0,0 +1,56 @@
+import { env } from "@/lib/env";
+import {
+  DeleteObjectsCommand,
+  type DeleteObjectsRequest,
+  ListObjectsV2Command,
+  S3Client,
+} from "@aws-sdk/client-s3";
+
+const client = new S3Client({
+  region: env.BETTER_UPLOAD_REGION,
+  endpoint: env.BETTER_UPLOAD_HOST,
+  credentials: {
+    accessKeyId: env.BETTER_UPLOAD_KEY_ID,
+    secretAccessKey: env.BETTER_UPLOAD_SECRET,
+  },
+  forcePathStyle: true,
+});
+
+async function main() {
+  // wipe all objects in the bucket
+  console.log(
+    `Wiping all objects in bucket "${env.BETTER_UPLOAD_BUCKET_NAME}"...`
+  );
+  let ContinuationToken: string | undefined;
+  do {
+    const listResponse = await client.send(
+      new ListObjectsV2Command({
+        Bucket: env.BETTER_UPLOAD_BUCKET_NAME,
+        ContinuationToken,
+      })
+    );
+
+    if (listResponse.Contents && listResponse.Contents.length > 0) {
+      const deleteParams: DeleteObjectsRequest = {
+        Bucket: env.BETTER_UPLOAD_BUCKET_NAME,
+        Delete: {
+          Objects: listResponse.Contents.map((obj) => ({ Key: obj.Key })),
+          Quiet: true,
+        },
+      };
+      await client.send(new DeleteObjectsCommand(deleteParams));
+      console.log(`Deleted ${deleteParams.Delete?.Objects?.length} objects...`);
+    }
+
+    ContinuationToken = listResponse.IsTruncated
+      ? listResponse.NextContinuationToken
+      : undefined;
+  } while (ContinuationToken);
+
+  console.log("Bucket wipe complete.");
+}
+
+main().catch((err) => {
+  console.error("Failed to wipe bucket:", err);
+  process.exit(1);
+});
